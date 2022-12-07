9a73725ea24faeb1bdb99148b3211d62b37d8b93
truongminhphongUIT <phongtm2002@gmail.com>
2022-12-08 01:52:24 +0700

    fix_button-right-click_success_and_end_product-detail

--- resources/views/products/content.blade.php
+++ resources/views/products/content.blade.php
@@ -14,68 +14,70 @@
 			</a>
 
 			<span class="stext-109 cl4">
-				Lightweight Jacket
+				{{$product->name}}
 			</span>
 		</div>
 	</div>
 
+	<!-- Product Detail -->
 	<section class="sec-product-detail bg0 p-t-65 p-b-60">
 		<div class="container">
 			<div class="row">
 				<div class="col-md-6 col-lg-7 p-b-30">
 					<div class="p-l-25 p-r-30 p-lr-0-lg">
 						<div class="wrap-slick3 flex-sb flex-w">
-							<div class="wrap-slick3-dots">
-								<ul class="slick3-dots" role="tablist" style="">
-									<li class="slick-active" role="presentation">
-										<img src=" {{$product->image}} ">
-										<div class="slick3-dot-overlay"></div>
-									</li>
-								</ul>
-							</div>
-							<div class="wrap-slick3-arrows flex-sb-m flex-w">
-								<button class="arrow-slick3 prev-slick3 slick-arrow" style="">
-									<i class="fa fa-angle-left" aria-hidden="true"></i>
-								</button>
+							<div class="wrap-slick3-dots"></div>
+							<div class="wrap-slick3-arrows flex-sb-m flex-w"></div>
 
-								<button class="arrow-slick3 next-slick3 slick-arrow" style="">
-									<i class="fa fa-angle-right" aria-hidden="true"></i>
-								</button>
-							</div>
-
-							<div class="slick3 gallery-lb slick-initialized slick-slider slick-dotted">
-								<div class="slick-list draggable"><div class="slick-track" style="opacity: 1; width: 1347px;"><div class="item-slick3 slick-slide slick-current slick-active" data-thumb="/template/images/product-detail-01.jpg" data-slick-index="0" aria-hidden="false" tabindex="0" role="tabpanel" id="slick-slide10" aria-describedby="slick-slide-control10" style="width: 449px; position: relative; left: 0px; top: 0px; z-index: 999; opacity: 1;">
+							<div class="slick3 gallery-lb">
+								<div class="item-slick3" data-thumb="{{$product->image}}">
 									<div class="wrap-pic-w pos-relative">
-										<img height= '420px' width = '420px'src="{{$product->image}}" alt="IMG-PRODUCT">
+										<img src="{{$product->image}}" alt="IMG-PRODUCT">
 
-										<a class="flex-c-m size-108 how-pos1 bor0 fs-16 cl10 bg0 hov-btn3 trans-04" href="/template/images/product-detail-01.jpg" tabindex="0">
+										<a class="flex-c-m size-108 how-pos1 bor0 fs-16 cl10 bg0 hov-btn3 trans-04" href="{{$product->image}}">
+											<i class="fa fa-expand"></i>
+										</a>
+									</div>
+								</div>
+
+								<div class="item-slick3" data-thumb="{{$product->image}}">
+									<div class="wrap-pic-w pos-relative">
+										<img src="{{$product->image}}" alt="IMG-PRODUCT">
+
+										<a class="flex-c-m size-108 how-pos1 bor0 fs-16 cl10 bg0 hov-btn3 trans-04" href="{{$product->image}}">
+											<i class="fa fa-expand"></i>
+										</a>
+									</div>
+								</div>
+
+								<div class="item-slick3" data-thumb="{{$product->image}}">
+									<div class="wrap-pic-w pos-relative">
+										<img src="{{$product->image}}" alt="IMG-PRODUCT">
+
+										<a class="flex-c-m size-108 how-pos1 bor0 fs-16 cl10 bg0 hov-btn3 trans-04" href="{{$product->image}}">
 											<i class="fa fa-expand"></i>
 										</a>
 									</div>
 								</div>
 							</div>
-						</div>		
+						</div>
 					</div>
 				</div>
-			</div>
-		</div>
 					
 				<div class="col-md-6 col-lg-5 p-b-30">
 					<div class="p-r-50 p-t-5 p-lr-0-lg">
 						<h4 class="mtext-105 cl2 js-name-detail p-b-14">
-							{{$title}}
+							{{$product->name}}
 						</h4>
 
 						<span class="mtext-106 cl2">
-							{!! \App\Helpers\Helper::price($product->price, $product->price_sale)!!}
+							{{$product->price_sale}}
 						</span>
 
 						<p class="stext-102 cl3 p-t-23">
-							{{$product->description}}
+							{{$product->description}}	
 						</p>
 						
-						<!--  -->
-						<div class="p-t-33">
 							<div class="flex-w flex-r-m p-b-10">
 								<div class="size-204 flex-w flex-m respon6-next">
 									<div class="wrap-num-product flex-w m-r-20 m-tb-10">
@@ -145,7 +147,7 @@
 						<div class="tab-pane fade show active" id="description" role="tabpanel">
 							<div class="how-pos2 p-lr-15-md">
 								<p class="stext-102 cl6">
-									{!!$product->content!!}
+									{{$product->content}}
 								</p>
 							</div>
 						</div>
@@ -297,7 +299,6 @@
 		</div>
 
 		<div class="bg6 flex-c-m flex-w size-302 m-t-73 p-tb-15">
-
 			<span class="stext-107 cl6 p-lr-25">
 				Categories: {{$product->menu->name}}
 			</span>

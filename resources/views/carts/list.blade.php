d527267a9f300ffc589cf151aa185740eae9cc1a
Thái Tăng Đức <ducthai1@gmail.com>
2022-12-11 14:44:22 +0700

    a

--- resources/views/carts/list.blade.php
+++ resources/views/carts/list.blade.php
@@ -3,6 +3,7 @@
 @section('content')
 <form class="bg0 p-t-130 p-b-85" method="post">
 		<div class="container">
+			@include('admin.alert')
 			<div class="row">
 				<div class="col-lg-10 col-xl-7 m-lr-auto m-b-50">
 					<div class="m-l-15 m-r--40 m-lr-0-xl">
@@ -10,6 +11,7 @@
 							@php
 							$total = 0;
 							@endphp
+							
 							@if (count($products)!=0)
 							<table class="table-shopping-cart">
 								<tr class="table_head">
@@ -84,9 +86,38 @@
 				</div>
 				<div class="col-sm-10 col-lg-7 col-xl-5 m-lr-auto m-b-50">
 					<div class="bor10 p-lr-40 p-t-30 p-b-40 m-l-63 m-r-40 m-lr-0-xl p-lr-15-sm">
-						<h4 class="mtext-109 cl2 p-b-30">
+						<h4 class="mtext-109 cl2 p-b-10 ">
 							Cart Totals
 						</h4>
+
+							<div class="size-100 p-r-18 p-r-0-sm w-full-ssm">				
+								<div class="p-t-15">
+									<span class="stext-112 cl8">
+										Thông Tin Khách Hàng
+									</span>
+
+									<div class="bor8 bg0 m-b-12">
+										<input class="stext-111 cl8 plh3 size-111 p-lr-15" type="text" name="name" placeholder="Tên Khách Hàng" required>
+									</div>
+
+									<div class="bor8 bg0 m-b-12">
+										<input class="stext-111 cl8 plh3 size-111 p-lr-15" type="text" name="phone" placeholder="Số Điện Thoại" required>
+									</div>
+
+									<div class="bor8 bg0 m-b-12">
+										<input class="stext-111 cl8 plh3 size-111 p-lr-15" type="text" name="address" placeholder="Địa Chỉ Giao Hàng">
+									</div>
+
+									<div class="bor8 bg0 m-b-12">
+										<input class="stext-111 cl8 plh3 size-111 p-lr-15" type="text" name="email" placeholder="Email Liên Hệ">
+									</div>
+
+									<div class="bor8 bg0 m-b-12">
+										<textarea class="stext-111 cl8 plh3 size-111 p-lr-15" name="content"></textarea>
+									</div>
+
+								</div>
+							</div>
 
 						<div class="flex-w flex-t p-t-27 p-b-33">
 							<div class="size-208">
@@ -103,7 +134,7 @@
 						</div>
 
 						<button class="flex-c-m stext-101 cl0 size-116 bg3 bor14 hov-btn3 p-lr-15 trans-04 pointer">
-							Proceed to Checkout
+							Đặt Hàng
 						</button>
 					</div>
 				</div>
